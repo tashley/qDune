ef392a070fed07a6add1bf2b6f8e182890717909
Tom <tashley22@gmail.com>
2016-08-31 11:54:12 -0600

    added manifest

---
+++ README.txt
@@ -0,0 +1,4 @@
+# qdune
+
+Tools for processing raw (point cloud) multibeam bathymetric surveys of
+migrating bed forms.
--- setup.py
+++ setup.py
@@ -1,24 +1,12 @@
+from distutils.core import setup
 
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-def setupPackage():
-
-    setup(name='qdune',
-          version='0.1.0',
-          description='dune point cloud processing tools',
-          url='https://github.com/tashley/qDune',
-          author='Thomas Ashley',
-          author_email='tashley22@gmail.com',
-          license='MIT',
-          packages=['qdune'],
-          zip_safe=False,
-          package_data={'qdune': ['*.xyz', '*.csv']})
-
-
-if __name__ == '__main__':
-    setupPackage()
+setup(name='qdune',
+      version='0.1.1',
+      description='dune point cloud processing tools',
+      author='Thomas Ashley',
+      author_email='tashley22@gmail.com',
+      url='https://github.com/tashley/qDune',
+      license='MIT',
+      packages=['qdune'],
+      package_data={'qdune': ['*.xyz', '*.csv']})
